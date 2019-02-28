10ab81f6dcdf37c5bf7b71b74ba7ce47bd5cc3d7
gitrat <ahenaoes@gmail.com>
2019-02-28 13:38:12 -0500

    maquetacion-basica

---
+++ js/global.js
@@ -0,0 +1,43 @@
+const header=`
+    <nav class="navbar navbar-light bg-light navbar-expand-lg">
+  		<a class="navbar-brand " href="/">Registradora app</a>
+		<div class='navbar-nav ml-auto'>
+			<a class='navbar-item ' href='/resumen'>Resumen<a/> 
+			<a class='navbar-item ml-4' href='/activo'>Activo<a/> 
+			<a class='navbar-item ml-4' href='/pasivo'>Pasivo<a/>
+
+			<a class='navbar-item ml-4 mr-5' href='#'>Informes<a/> 
+			<a class='navbar-item ml-5 mr-5' href='#'>Config<a/> 
+		</div>
+		
+	</nav>
+
+
+`
+
+$('body').prepend(header)
+
+const countList= `
+	  			<option value=cuenta-1>cuenta-1</option>
+				<option value=cuenta-2>cuenta-2</option>
+`
+
+$('.count-select ').html(countList)
+
+const partitionList=`
+
+	  			<option value="particion 1">particion1</option>
+				<option value="particion 2">particion 2</option>
+
+`
+
+$('.particion-select ').prepend(partitionList)
+$('#partitionList ').prepend(partitionList)
+
+//handle radio inputs unchecked
+$('input[type=radio]').click(function(){
+    if (this.previous) {
+        this.checked = false;
+    }
+    this.previous = this.checked;
+});
