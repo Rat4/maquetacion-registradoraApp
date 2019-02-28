effee9d51a1e121a9ffde01141fa89f4ac79a924
Rat4 <32190100+Rat4@users.noreply.github.com>
2019-02-28 13:51:36 -0500

    Update global.js

--- js/global.js
+++ js/global.js
@@ -2,7 +2,7 @@ const header=`
     <nav class="navbar navbar-light bg-light navbar-expand-lg">
   		<a class="navbar-brand " href="/">Registradora app</a>
 		<div class='navbar-nav ml-auto'>
-			<a class='navbar-item 'href='https://rat4.github.io/maquetacion-registradoraApp/resumen.html'>Resumen<a/> 
+			<a class='navbar-item 'href='*/resumen.html'>Resumen<a/> 
 			<a class='navbar-item ml-4'https://rat4.github.io/maquetacion-registradoraApp/activo.html'>Activo<a/> 
 			<a class='navbar-item ml-4'https://rat4.github.io/maquetacion-registradoraApp/pasivo.html'>Pasivo<a/>
 
